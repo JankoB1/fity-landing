23a44090001eca3c0036d1e68b8a1db0fd5786b1
Janko Blagojevic <janko.tbbt@gmail.com>
2025-08-26 15:40:57 +0200

    about us change

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -9,12 +9,12 @@ function Thanks() {
             <div className="privacy-page">
                 <div className="policy-top">
                     <img src={logo} className="hero-logo w-[65px]" alt="logo"/>
-                    <a href="/"><img src={arrow} alt=""/>Vrati se na početnu stranicu</a>
+                    {/*<a href="/"><img src={arrow} alt=""/>Vrati se na početnu stranicu</a>*/}
                 </div>
                 <h1 style={{ paddingTop: '200px', textAlign: 'center' }}>O nama</h1>
-                <p style={{ textAlign: 'center', paddingBottom: '150px' }}>Fity je prva aplikacija u regionu koja na lokalnom jeziku kreira personalizovane planove ishrane za vaše ciljeve – brzo, jednostavno i povoljno. U samo nekoliko minuta dobijate jelovnik prilagođen vašim navikama i preferencama, sa zdravim, ukusnim i praktičnim receptima.</p>
+                <p style={{ textAlign: 'center' }}>Fity je prva aplikacija u regionu koja na lokalnom jeziku kreira personalizovane planove ishrane za vaše ciljeve – brzo, jednostavno i povoljno. U samo nekoliko minuta dobijate jelovnik prilagođen vašim navikama i preferencama, sa zdravim, ukusnim i praktičnim receptima.</p>
                 <p style={{ textAlign: 'center' }}>Pored recepata, aplikacija vam nudi alate za planiranje obroka, liste za kupovinu, vođenje dnevnika ishrane i praćenje napretka – sve na jednom mestu. Bez komplikovanih dijeta, odricanja i gladovanja.</p>
-                <p style={{ textAlign: 'center' }}>Fity su kreirali nutricionisti, lekari, treneri i kulinarski stručnjaci kako bi vam omogućili bezbedan i održiv put ka boljem zdravlju, formi i energiji. Naš cilj je da pravilna ishrana postane jednostavna i dostupna svima – za dugoročne rezultate i kvalitetniji život.</p>
+                <p style={{ textAlign: 'center', paddingBottom: '150px' }}>Fity su kreirali nutricionisti, lekari, treneri i kulinarski stručnjaci kako bi vam omogućili bezbedan i održiv put ka boljem zdravlju, formi i energiji. Naš cilj je da pravilna ishrana postane jednostavna i dostupna svima – za dugoročne rezultate i kvalitetniji život.</p>
             </div>
             <Footer></Footer>
         </>
